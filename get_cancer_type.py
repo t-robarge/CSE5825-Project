74e5d78aa1a961f019cd2a07b2d45a86c8fffdee
trobarge <timothy.robarge@uconn.edu>
2025-12-06 20:17:53 -0500

    update to get_cancer_type

--- get_cancer_type.py
+++ get_cancer_type.py
@@ -5,6 +5,7 @@ clin = pd.read_csv("clinical_PANCAN_patient_with_followup.tsv", sep="\t",
                    encoding="latin1")
 # tumor x mutation matrix
 mut_matrix = pd.read_csv("sbs96_by_tumor.csv", index_col=0)
+con_matrix = pd.read_csv("consequence_by_tumor.csv", index_col=0)
 # keep only what we need
 clin_small = clin[["bcr_patient_barcode", "acronym"]].drop_duplicates()
 # bcr_patient_barcode: e.g. TCGA-OR-A5J1
@@ -18,7 +19,12 @@ patient_to_acronym = dict(
 annotated_matrix = mut_matrix.copy()
 annotated_matrix["Cancer_Type"] = annotated_matrix.index.str.slice(0, 12).map(patient_to_acronym)
 
+annotated_con_matrix = con_matrix.copy()
+annotated_con_matrix["Cancer_Type"] = annotated_con_matrix.index.str.slice(0, 12).map(patient_to_acronym)
+
 # save annotated matrix
 annotated_matrix.to_csv("sbs96_by_tumor_with_cancer_type.csv")
+annotated_con_matrix.to_csv("consequence_by_tumor_with_cancer_type.csv")
 
 #print cancer types
+print(annotated_matrix["Cancer_Type"].value_counts())
